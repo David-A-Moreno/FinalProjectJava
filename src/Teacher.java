329a588b106ac5abba02b8fc002f39b7d60be975
David Andres Moreno Chaves <david.moreno01@globant.com>
2025-02-23 13:49:58 -0500

    the class teacher was created

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,4 +1,27 @@
 
-public abstract class Teacher {
+public abstract class Teacher extends Person {
+
+protected double baseSalary;
+    
+    public Teacher(String name, int age, double baseSalary) {
+        super(name, age);
+        this.baseSalary = baseSalary;
+    }
+    
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+    
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+    
+    // Método abstracto para calcular el salario, que se implementará en las subclases
+    public abstract double calculateSalary();
+    
+    @Override
+    public void printInfo() {
+        System.out.println("Teacher - Name: " + name + ", Base Salary: " + baseSalary + ", Salary: " + calculateSalary());
+    }
 
 }

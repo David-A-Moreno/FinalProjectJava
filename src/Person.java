e08394ba72da3c295fa35b2540ea7afb63c60b88
David Andres Moreno Chaves <david.moreno01@globant.com>
2025-02-23 13:29:12 -0500

    an abstract method to print info in person class was added, and the student class was created

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,14 @@
 
 public abstract class Person {
-	private String name;
+	protected String name;
 	private int age;
 	
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
+	
+	public abstract void printInfo();
 
 	public String getName() {
 		return name;

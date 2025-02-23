952e46b0c8deaa57c4ce6e73332c476c322b18db
David Andres Moreno Chaves <david.moreno01@globant.com>
2025-02-23 15:10:15 -0500

    the class that manages the university classes was created

---
+++ src/UniversityClass.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityClass {
+	
+	private static int classCount = 0;
+    private String className;
+    private String classroom;
+    private Teacher teacher;
+    private List<Student> students;
+    
+    public UniversityClass(String className, String classroom, Teacher teacher) {
+        this.className = className;
+        this.classroom = classroom;
+        this.teacher = teacher;
+        this.students = new ArrayList<>();
+        classCount++;
+    }
+    
+    public String getClassName() {
+        return className;
+    }
+    
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    
+    public String getClassroom() {
+        return classroom;
+    }
+    
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+    
+    public Teacher getTeacher() {
+        return teacher;
+    }
+    
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    
+    public List<Student> getStudents() {
+        return students;
+    }
+    
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+    
+    public void printClassInfo() {
+        System.out.println("Class: " + className + ", Classroom: " + classroom);
+        System.out.println("Teacher:");
+        teacher.printInfo();
+        System.out.println("Students:");
+        for (Student s : students) {
+            s.printInfo();
+        }
+    }
+    
+    public static int getClassCount() {
+        return classCount;
+    }
+}
